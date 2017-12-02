171937aa557fd3f7ad8cbea5bb9280b40034e677
ChristopheClaustre <christophe.claustre.31@gmail.com>
2017-12-02 20:08:56 +0100

    [FCT] ajout du script pour gérer le gamestate

---
+++ Assets/Script/GameState.cs
@@ -0,0 +1,150 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class GameState :
+    MonoBehaviour
+{
+    #region Sub-classes/enum
+    /***************************************************/
+    /***  SUB-CLASSES/ENUM      ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    public enum EnumInput
+    {
+        eBlank = 0,
+        eLeft = -1,
+        eRight = 1,
+        eNbInput = 3
+    }
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    public int AlcoolState
+    {
+        get { return m_alcoolState; }
+    }
+    public int PassengerAlcoolState
+    {
+        get { return m_passengerAlcoolState; }
+    }
+    public int PassengerFearState
+    {
+        get { return m_passengerFearState; }
+    }
+    public int MaxPassengerFearState
+    {
+        get { return m_maxPassengerFearState; }
+    }
+    public float RemainingDistance
+    {
+        get { return m_remainingDistance; }
+    }
+    public int Vitesse
+    {
+        get { return m_vitesse; }
+    }
+    public int DirtyState
+    {
+        get { return m_dirtyState; }
+    }
+    public float Lags
+    {
+        get { return m_lags; }
+    }
+    public EnumInput CurrentFrameInput
+    {
+        get { return m_listInput[0]; }
+    }
+
+    /********  PROTECTED        ************************/
+
+    #endregion
+    #region Constants
+    /***************************************************/
+    /***  CONSTANTS             ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    [SerializeField, Range(0, 10)] private int m_alcoolState = 0;
+    [SerializeField, Range(0, 10)] private int m_passengerAlcoolState = 0;
+    [SerializeField, Range(100, 0)] private int m_passengerFearState = 0;
+    [SerializeField, Range(2000, 0)] private float m_remainingDistance = 2000;
+    [SerializeField, Range(0, 10)] private int m_vitesse = 0;
+    [SerializeField, Range(0, 10)] private int m_dirtyState = 0;
+    [Header("Initialization Only :")]
+    [SerializeField, Range(50, 100)] private int m_maxPassengerFearState = 50;
+    [SerializeField, Range(0.0f, 1.0f)] private float m_lags = 0;
+    private List<EnumInput> m_listInput = new List<EnumInput>();
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        for (int i = 0; i < Mathf.RoundToInt(m_lags / Time.deltaTime) ; ++i) // << CACA !!!!!!!
+        {
+            m_listInput.Add(EnumInput.eBlank);
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // input
+        m_listInput.Add((EnumInput) Mathf.RoundToInt(Input.GetAxis("Move car")));
+        m_listInput.RemoveAt(0);
+
+        // distance
+        m_remainingDistance -= m_vitesse * Time.deltaTime;
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
