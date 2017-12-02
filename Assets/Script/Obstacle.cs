2d18b7b832058962aabcbe8cff63001c6a59fa42
Xavier Chalut <xavier.chalut@gmail.com>
2017-12-02 22:57:15 +0100

    [FCT] Add Obstacle script and the poubelle obstacle exemple object

---
+++ Assets/Script/Obstacle.cs
@@ -0,0 +1,80 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class Obstacle :
+    MonoBehaviour
+{
+
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    /********  INSPECTOR        ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    [SerializeField]
+    private float m_obstacleWidth;
+    [SerializeField]
+    private float m_fearEffect;
+    [SerializeField]
+    private bool m_deadlyTrap;
+
+    private BoxCollider m_obstacleCollider;
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        m_obstacleCollider = gameObject.AddComponent<BoxCollider>();
+        m_obstacleCollider.size = new Vector3(m_obstacleWidth,1.0f,1.0f);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider p_touchedCollider)
+    {
+        if (p_touchedCollider.CompareTag("Car"))
+        {
+            if (m_deadlyTrap)
+            {
+                //End game on game stat
+            }
+            else
+            {
+                //Increase fear on game stat (use m_fearEffect value)
+            }
+        }
+        
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
