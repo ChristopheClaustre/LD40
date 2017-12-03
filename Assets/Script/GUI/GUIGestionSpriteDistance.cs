46263e73ad19c95311954823d7829e8f4efed1e3
ChristopheClaustre <christophe.claustre.31@gmail.com>
2017-12-03 06:11:04 +0100

    [FCT] ajout de script pour de l'HUD

---
+++ Assets/Script/GUI/GUIGestionSpriteDistance.cs
@@ -0,0 +1,44 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class GUIGestionSpriteDistance :
+    MonoBehaviour
+{
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x = ONEGameState.Instance.CurrentDistance / ONEGameState.Instance.MaxDistance;
+        transform.localScale = scale;
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
---
+++ Assets/Script/GUI/GUIGestionSpriteInput.cs
@@ -0,0 +1,42 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class GUIGestionSpriteInput :
+    MonoBehaviour
+{
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        gameObject.GetComponent<Animator>().SetFloat("Input", Input.GetAxis("Move car"));
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
