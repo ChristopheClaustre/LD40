17970f850faa124cad2443bd7cdd41b344be1542
ChristopheClaustre <christophe.claustre.31@gmail.com>
2017-12-04 06:36:27 +0100

    [FCT/WIP] script randomisation hauteur building

---
+++ Assets/Script/IAmARandomFloorBuilding.cs
@@ -0,0 +1,104 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class IAmARandomFloorBuilding :
+    MonoBehaviour
+{
+    #region Sub-classes/enum
+    /***************************************************/
+    /***  SUB-CLASSES/ENUM      ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    #endregion
+    #region Constants
+    /***************************************************/
+    /***  CONSTANTS             ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    /********  INSPECTOR        ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+     
+    [SerializeField] private GameObject m_gameObject;
+    [SerializeField, Range(2, 10)] private int m_minFloors = 2;
+    [SerializeField, Range(2, 10)] private int m_maxFloors = 4;
+    [SerializeField] private float m_increment;
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        AddOneFloor(2*m_increment);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    public void AddOneFloor(float p_yPosition)
+    {
+        if (!m_gameObject)
+            return;
+        
+        GameObject floor = Instantiate(m_gameObject, transform);
+        Vector3 position = floor.transform.localPosition;
+        position.y = p_yPosition;
+        floor.transform.localPosition = position;
+    }
+
+    #endregion
+}
