5a49200a5e6be4221b4dd3d25a74bf1c00be4062
ChristopheClaustre <christophe.claustre.31@gmail.com>
2017-12-04 16:49:40 +0100

    [FCT] déplacement du listener sur la voiture + ajout de la jauge de peur

---
+++ Assets/Script/Partie II/GUI/GUIGestionSpritePeur.cs
@@ -0,0 +1,44 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class GUIGestionSpritePeur :
+    MonoBehaviour
+{
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x = ONEGameState.Instance.PassengerFearState / ONEGameState.Instance.MaxPassengerFearState;
+        transform.localScale = scale;
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
